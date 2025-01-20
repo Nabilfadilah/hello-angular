055580e36e42858eeb490fa6e12b12e7234429d9
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-01-20 21:29:05 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
+// File TypeScript utama yang berisi logika aplikasi.
+
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
+  // metedata
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'hello-angular';
+  // property
+  title = 'Hello, World! nabil ganteng';
 }
